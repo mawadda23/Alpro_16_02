acacc439be2e619df98af91842d45d81546e49e9
Alpro_16_02 <mawaddawarohma2@gmail.com>
2020-05-08 15:12:14 +0700

    Commit pertama

---
+++ ch02/Jurusan.java
@@ -0,0 +1,17 @@
+package ch02;
+
+pubic class Jurusan {
+	String nama;
+	int JumlahMahasiswa;
+	String akreditasi;
+	
+	public String getNama() {
+		return nama;
+	}
+	public String getJumalahMahasiswa() {
+		return jumlahMahasiswa;
+	}
+	public String getAkreditasi() {
+		return akreditasi;
+	}
+}
---
+++ ch02/Mahasiswa.java
@@ -0,0 +1,31 @@
+package ch02;
+
+public clas Mahasiswa {
+	String nim;
+	String nama;
+	String alamat;
+	String hp;
+	
+	public String getNim() {
+		return nim;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	
+	public String getAlamat() {
+		return alamat;
+	}
+	
+	public String getHp() {
+		return hp;
+	}
+	
+	public void cetak() {
+		System.out.println("NIM : " + nim);
+		System.out.println("Nama : " + nama);
+		System.out.println("Alamat : " + alamat);
+		System.our.println("HP : " + hp);
+	}
+}
